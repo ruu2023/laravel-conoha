f6f5f5e01eea3268c1c0ab489830589aa3d5be44
ruu2023 <kokihiru2038@gmail.com>
2026-08-06 20:50:00 +0900

    gtag

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,18 +1,33 @@
-import { createInertiaApp } from "@inertiajs/react";
+import { createInertiaApp, router } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createRoot } from "react-dom/client";
 import type { ComponentType } from "react";
 
-createInertiaApp({
-    resolve: async (name) => {
-        const module = await resolvePageComponent<{ default: ComponentType }>(
-            `./Pages/${name}.tsx`,
-            import.meta.glob<{ default: ComponentType }>("./Pages/**/*.tsx"),
-        );
+declare global {
+  interface Window {
+    gtag?: (...args: unknown[]) => void;
+  }
+}
 
-        return module.default;
-    },
-    setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />);
-    },
+router.on("navigate", (event) => {
+  if (typeof window.gtag === "function") {
+    window.gtag("event", "page_view", {
+      page_path: event.detail.page.url,
+      page_title: document.title,
+    });
+  }
+});
+
+createInertiaApp({
+  resolve: async (name) => {
+    const module = await resolvePageComponent<{ default: ComponentType }>(
+      `./Pages/${name}.tsx`,
+      import.meta.glob<{ default: ComponentType }>("./Pages/**/*.tsx"),
+    );
+
+    return module.default;
+  },
+  setup({ el, App, props }) {
+    createRoot(el).render(<App {...props} />);
+  },
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,6 +23,15 @@
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx'])
         @inertiaHead
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SWCLP4WEZ5"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-SWCLP4WEZ5', { send_page_view: false }); // 自動送信をOFF
+        </script>
     </head>
     <body class="antialiased">
         @inertia
